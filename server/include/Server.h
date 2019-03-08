16afda332cb36abec79820f59e7cf4f10e0890fd
horak_000 <horakj7@gmail.com>
2019-03-08 16:29:39 +0100

    Api update

--- server/include/Server.h
+++ server/include/Server.h
@@ -5,15 +5,40 @@
 #ifndef HELLO_WORLD_SERVER_H
 #define HELLO_WORLD_SERVER_H
 
-class Server {
+struct Request {
+    RequestType type;
+};
 
-public:
-
-
+struct Response {
+    bool successful;
+    size_t size;
 
 };
 
 
+class Server {
+
+public:
+
+    /**
+     * Handle incoming request on user port
+     * @param request from user
+     * @return response data
+     */
+    Response handleUserRequest(const Request &request);
+
+    /**
+     * Handle incoming request for system on system port
+     * @return connection id
+     */
+    long establishConnection();
+
+    /**
+     * Terminate connection
+     * @param cid connection id
+     */
+    void terminateConnection(long cid);
+};
 
 
 #endif //HELLO_WORLD_SERVER_H
