be14b368fe18676b1912e85ad288beb77ec8d774
horak_000 <horakj7@gmail.com>
2019-03-14 12:31:20 +0100

    Rsa keygen impl

--- src/shared/rsa_2048.h
+++ src/shared/rsa_2048.h
@@ -42,6 +42,7 @@ class RSA2048 : public AsymmetricCipher {
 
     const static int KEY_SIZE = 2048;
     const static int MAX_DATA_SIZE = 245;
+    const static int EXPONENT = 65537;
 
     mbedtls_pk_context key{};
 
@@ -54,6 +55,9 @@ public:
         if (mbedtls_pk_setup(&key, mbedtls_pk_info_from_type( MBEDTLS_PK_RSA )) != 0) {
             throw std::runtime_error("Could not initialize RSA ciper.");
         }
+        //set OAEP padding
+        mbedtls_rsa_set_padding((mbedtls_rsa_context *) (key).pk_ctx, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA512);
+
     }
 
     ~RSA2048() override {
@@ -74,23 +78,16 @@ public:
     std::string generateKeyPair() override {
         Random random{};
         mbedtls_ctr_drbg_context* random_ctx = random.getEngine();
-
         if (!valid()) throw std::runtime_error("Could not generate RSA key.");
         auto* rsa_ctx = (mbedtls_rsa_context *) (key).pk_ctx;
 
-        int exponent = static_cast<int>(std::pow(2, random.getBounded(4, 18)));
-        exponent += 1; //primes
-
-        std::cout << exponent << "\n";
-
-        if (mbedtls_rsa_gen_key( rsa_ctx, mbedtls_ctr_drbg_random, random_ctx, KEY_SIZE, exponent ) != 0) {
+        if (mbedtls_rsa_gen_key( rsa_ctx, mbedtls_ctr_drbg_random, random_ctx, KEY_SIZE, EXPONENT) != 0) {
             throw std::runtime_error("Could not generate RSA key.");
         }
 
         unsigned char buffer_public[MBEDTLS_MPI_MAX_SIZE];
         std::cout << "length: " << MBEDTLS_MPI_MAX_SIZE << "\n";
-
-        if (mbedtls_pk_write_pubkey_pem( &key, buffer_public, MBEDTLS_MPI_MAX_SIZE) != 0) {
+        if (mbedtls_pk_write_pubkey_pem(&key, buffer_public, MBEDTLS_MPI_MAX_SIZE) != 0) {
             throw std::runtime_error("Could not write public key.");
         }
         int keylen = getKeyLength(buffer_public, MBEDTLS_MPI_MAX_SIZE, "-----END PUBLIC KEY-----");
@@ -102,18 +99,16 @@ public:
         out_pub.write((char *)buffer_public, keylen);
 
 
-
-
         unsigned char buffer_private[MBEDTLS_MPI_MAX_SIZE * 2];
         std::cout << "length: " << MBEDTLS_MPI_MAX_SIZE * 2 << "\n";
 
-        if (int res = mbedtls_pk_write_key_pem( &key, buffer_private, MBEDTLS_MPI_MAX_SIZE * 2) != 0) {
+        if (int res = mbedtls_pk_write_key_pem(&key, buffer_private, MBEDTLS_MPI_MAX_SIZE * 2) != 0) {
             std::cout << "Error code " << std::hex << res << "\n";
-            throw std::runtime_error("Could not write public key.");
+            throw std::runtime_error("Could not write private key.");
         }
         keylen = getKeyLength(buffer_private, MBEDTLS_MPI_MAX_SIZE * 2, "-----END RSA PRIVATE KEY-----");
         if (keylen == 0) {
-            throw std::runtime_error("Generated public key damaged.");
+            throw std::runtime_error("Generated private key damaged.");
         }
         std::cout << "written: " << keylen << "\n";
         std::ofstream out_pri{"private.txt", std::ios::binary};
