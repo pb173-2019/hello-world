ac40fea77144d1c091998fb075f300df176cc59d
horak_000 <horakj7@gmail.com>
2019-03-15 13:38:49 +0100

    Clang compiles

--- test/shared/test_rsa.cpp
+++ test/shared/test_rsa.cpp
@@ -8,16 +8,16 @@ using namespace helloworld;
 TEST_CASE("Rsa keygen") {
     RSAKeyGen keyGen;
 
-//    keyGen.savePublicKey("pub.pem");
-//    keyGen.savePrivateKey("priv.pem", "123");
+    keyGen.savePublicKey("pub.pem");
+    keyGen.savePrivateKey("priv.pem", nullptr);
 
-//    RSA2048 rsa;
-//    rsa.loadPublicKey("pub.pem");
-//    std::vector<unsigned char> data = rsa.encrypt("My best message");
+    RSA2048 rsa;
+    rsa.loadPublicKey("pub.pem");
+    std::vector<unsigned char> data = rsa.encrypt("My best message");
 
-//    RSA2048 rsa2;
-//    rsa2.loadPrivateKey("priv.pem", nullptr);
-//    std::string res = rsa2.decrypt(data);
-//
-//    CHECK(res == "My best message");
+    RSA2048 rsa2;
+    rsa2.loadPrivateKey("priv.pem", nullptr);
+    std::string res = rsa2.decrypt(data);
+
+    CHECK(res == "My best message");
 }
