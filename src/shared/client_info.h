5f274d3c7bf62cdeff15fe7f86256a614bf47943
horak_000 <horakj7@gmail.com>
2019-03-16 11:43:42 +0100

    User info structure update & serialization support

---
+++ src/shared/client_info.h
@@ -0,0 +1,48 @@
+/**
+ * @file client_info.h
+ * @author Jiří Horák (469130@mail.muni.cz)
+ * @brief Client info structure to hold user data
+ * @version 0.1
+ * @date 16. 3. 2019
+ *
+ * @copyright Copyright (c) 2019
+ *
+ */
+
+#ifndef HELLOWORLD_SHARED_CLIENT_INFO_H_
+#define HELLOWORLD_SHARED_CLIENT_INFO_H_
+
+#include <string>
+#include <iostream>
+
+#include "serializable.h"
+
+namespace helloworld {
+
+struct UserData : Serializable<UserData> {
+    uint32_t id = 0;
+    bool online = false;
+    std::string name;
+    std::string publicKey;
+
+    std::vector<unsigned char> serialize() override {
+        std::vector<unsigned char> result;
+        Serializable::addNumeric<uint32_t>(result, id);
+        Serializable::addContainer<std::string>(result, name);
+        Serializable::addContainer<std::string>(result, publicKey);
+        return result;
+    }
+
+    static UserData deserialize(const std::vector<unsigned char>& data) {
+        UserData userData;
+        uint64_t position = 0;
+        position += Serializable::getNumeric<uint32_t>(data, 0, userData.id);
+        position += Serializable::getContainer<std::string>(data, position, userData.name);
+        Serializable::getContainer<std::string>(data, position, userData.publicKey);
+        return userData;
+    }
+};
+
+}
+
+#endif //HELLOWORLD_SHARED_CLIENT_INFO_H_
---
+++ test/shared/test_user_nfo.cpp
@@ -0,0 +1,23 @@
+#include "catch.hpp"
+
+#include "../../src/shared/client_info.h"
+
+using namespace helloworld;
+
+TEST_CASE("Serialization and deserialization") {
+
+    UserData data;
+    data.id = 6513246;
+    data.name = "Honza";
+    data.publicKey = "--- BEGIN PUBLIC KEY ---\n"
+                     "DJhgfvjadBVads65dG3adsaa\n"
+                     "SV56D1ASD3VEB5GASD353dga\n"
+                     "--- END PUBLIC KEY ---\n";
+
+    std::vector<unsigned char> ser = data.serialize();
+
+    UserData result = UserData::deserialize(ser);
+    CHECK(data.id == result.id);
+    CHECK(data.name == result.name);
+    CHECK(data.publicKey == result.publicKey);
+}
