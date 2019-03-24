76a9861bf1bf752f8d92263b226dec04f6fc97bc
horak_000 <horakj7@gmail.com>
2019-03-25 00:29:26 +0100

    Repair tests

--- test/shared/test_connection_manager.cpp
+++ test/shared/test_connection_manager.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Client parses request, server reads request no session key") {
     keyGenClient.savePrivateKeyPassword("client-priv.pem", "123456789");
     keyGenClient.savePublicKey("client-pub.pem");
 
-    ClientToServerManager client{"server_pub.pem", "client-priv.pem", "123456789"};
+    ClientToServerManager client{"server_pub.pem"};
     GenericServerManager server{"server_priv.pem", "323994cfb9da285a5d9642e1759b224a", "2b7e151628aed2a6abf7158809cf4f3c"};
 
     Request request{{Request::Type::LOGIN, 5, 28}, std::vector<unsigned char>{1,5,6,132,1,56,1,5,8}};
@@ -23,12 +23,12 @@ TEST_CASE("Client parses request, server reads request no session key") {
 }
 
 TEST_CASE("Client parses request, server reads request session key") {
-    ClientToServerManager client{"server_pub.pem", "client-priv.pem", "123456789"};
+    ClientToServerManager client{"server_pub.pem"};
     client.openSecureChannel("73bed6b8e3c1743b7116e69e22229516");
 
     ServerToClientManager server{"73bed6b8e3c1743b7116e69e22229516"};
 
-    Request request{{Request::Type::DELETE, 20, 1111}, std::vector<unsigned char>{5,15,15,1,99,32,13,13,15,6,51,32,13,2}};
+    Request request{{Request::Type::REMOVE, 20, 1111}, std::vector<unsigned char>{5,15,15,1,99,32,13,13,15,6,51,32,13,2}};
 
     Request result = server.parseIncoming(client.parseOutgoing(request));
     CHECK(result.header.type == request.header.type);
@@ -38,7 +38,7 @@ TEST_CASE("Client parses request, server reads request session key") {
 }
 
 TEST_CASE("Server parses response, client reads response session key") {
-    ClientToServerManager client{"server_pub.pem", "client-priv.pem", "123456789"};
+    ClientToServerManager client{"server_pub.pem"};
     client.openSecureChannel("73bed6b8e3c1743b7116e69e22229516");
 
     ServerToClientManager server{"73bed6b8e3c1743b7116e69e22229516"};
