05717fe4dbe2c8dc96ecfcfce6c7d25d512975fe
xmitruk <xmitruk@fi.muni.cz>
2019-04-12 15:50:29 +0200

    repaired wrongly resolved conflict

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -15,8 +15,8 @@ Client::Client(std::string username,
             const std::string &password)
     : _username(std::move(username)),
       _password(password),
-      _x3dh(std::make_unique<X3DH>(_username, _password)),
-      _transmission(std::make_unique<ClientFiles>(this, _username)) {
+      _x3dh(std::make_unique<X3DH>(_username, _password))
+       {
 
     _rsa.loadPrivateKey(clientPrivKeyFilename, password);
 }
